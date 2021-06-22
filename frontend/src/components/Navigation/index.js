cf68be925c7008390c204fafd3e818fb3035ba8e
Dalton Klossman <klossdal@gmail.com>
2021-06-22 12:25:14 -0400

    fixed frontend issues

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,38 @@
+// frontend/src/components/Navigation/index.js
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import LoginFormModal from '../LoginFormPage';
+import './Navigation.css';
+
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector(state => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <ProfileButton user={sessionUser} />
+    );
+  } else {
+    sessionLinks = (
+      <>
+        <LoginFormModal />
+        <NavLink to="/signup">Sign Up</NavLink>
+      </>
+    );
+  }
+
+  return (
+    <div className="nav">
+      <ul className="nav-container">
+        <li className='home-link'>
+          <NavLink exact to="/">Home</NavLink>
+          {isLoaded && sessionLinks}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
